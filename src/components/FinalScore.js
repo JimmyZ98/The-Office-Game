7c6c9997779c7ba5708ca3bf2841b7aaac3cf09d
Carlos Ocampo <charlie@carlosocampo.ca>
2022-06-10 13:52:38 -0500

    dynamic score working

---
+++ src/components/FinalScore.js
@@ -0,0 +1,8 @@
+
+function FinalScore({ score }) {
+    return ( 
+        <p>FINAL SCORE: {score}</p>
+     );
+}
+
+export default FinalScore;
--- src/pages/GamePage/GamePage.js
+++ src/pages/GamePage/GamePage.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Component } from "react";
 import ClockCounter from "../../components/ClockCounter/ClockCounter";
+import FinalScore from "../../components/FinalScore";
 import Header from "../../components/Header/Header";
 import Options from "../../components/Options/Options";
 import Quote from "../../components/Quote/Quote";
@@ -24,17 +25,19 @@ class GamePage extends Component {
         index: this.state.index + 1
       });
     }
-    axios.post(`http://localhost:8080/game/check${this.state.currentQuestion.id}` ,{
-      answer: e.target.value
+    axios.post(`http://localhost:8080/game/check${this.state.currentQuestion.id}`, {
+      answer: e.target.value,
+      timer: this.state.timer
     })
-    .then(res => {
-      this.setState({
-        score: res.data
+      .then(res => {
+        this.setState({
+          score: res.data,
+          timer: 10
+        })
       })
-    })
-    .catch(err =>{
-      console.log(err)
-    })
+      .catch(err => {
+        console.log(err)
+      })
     if (this.state.index = 5) {
       // input 5 and input name, leaderboard
       // index = 0, return homepage
@@ -43,15 +46,15 @@ class GamePage extends Component {
 
   componentDidMount() {
     axios.get('http://localhost:8080/game/start')
-    .then(res => {
-      this.setState({
-        questionArr: res.data,
-        currentQuestion: res.data[0]
+      .then(res => {
+        this.setState({
+          questionArr: res.data,
+          currentQuestion: res.data[0]
+        })
       })
-    })
-    .catch(err => {
-      console.error('could not download', err)
-    })
+      .catch(err => {
+        console.error('could not download', err)
+      })
     this.interval = setInterval(() => {
       if (this.state.timer > 0) {
         this.setState(({ timer }) => ({
@@ -73,12 +76,18 @@ class GamePage extends Component {
     return (
       <div className="wrapper">
         <Header />
-        <ClockCounter timer={this.state.timer} score={this.state.score} />
-        <Quote quote={this.state.currentQuestion?.quote || ""} />
-        <Options
-          options={this.state.currentQuestion?.options || ""}
-          handleAnswerOptionClick={this.handleAnswerOptionClick}
-        />
+        {this.state.index < 5 ?
+          <div>
+            <ClockCounter timer={this.state.timer} score={this.state.score} />
+            <Quote quote={this.state.currentQuestion?.quote || ""} />
+            <Options
+              options={this.state.currentQuestion?.options || ""}
+              handleAnswerOptionClick={this.handleAnswerOptionClick}
+            />
+          </div>
+          :
+          <FinalScore score={this.state.score} />
+        }
       </div>
     );
   }
