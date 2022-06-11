8f36408a87c2298f4ef1af5788352cb0e8383e23
Carlos Ocampo <charlie@carlosocampo.ca>
2022-06-11 09:03:06 -0500

    latest working version with some styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import GamePage from "./pages/GamePage/GamePage";
