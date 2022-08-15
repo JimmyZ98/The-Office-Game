57788cb55bca1ab7152023432bc8e3ce4af47293
JimmyZ98 <sam.jimmy.zhen@gmail.com>
2022-08-15 18:45:37 -0400

    styling changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./Header.scss";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChartSimple} from "@fortawesome/free-solid-svg-icons"
 
 function Header() {
   return (
     <header className="header">
       <div className="header__nav">
-        <Link to="/" className="header__logo"></Link>
-        <Link to="/game" className="header__leaderboard">
-          Play
-        </Link>
-        <Link to="/leaderboard" className="header__leaderboard">
-          Leaderboard
-        </Link>
+        <Link to="/" className="header__logo"><h1>Turntables</h1></Link>
+        <div className="header__leaderboard-container">
+          <Link to="/game" className="header__leaderboard">
+           Play
+          </Link>
+          <Link to="/leaderboard" className="header__leaderboard">
+            <FontAwesomeIcon icon={faChartSimple}/>
+          </Link>
+        </div>
       </div>
     </header>
   );
--- src/pages/IntroPage/IntroPage.js
+++ src/pages/IntroPage/IntroPage.js
@@ -4,22 +4,23 @@ import "./IntroPage.scss";
 
 function IntroPage() {
   return (
-    <div className="intro">
-      <h1>The Office Game</h1>
+    <div className="intro"> 
+    <div className="content-border">
       <div className="intro__rules rules">
         <h2 className="rules__title">Rules</h2>
-        <p>
+        <p className="intro__rules-text">
           The objective of the game is to identify the character who made the
           quote!
-        </p>
-        <p>Quotes will show up on the screen.</p>
-        <p>You will have 10 seconds to select your answer.</p>
-        <p>The faster you answer, the more points you will get.</p>
-        <p>Have fun!</p>
+        </p >
+        <p className="intro__rules-text">Quotes will show up on the screen.</p>
+        <p className="intro__rules-text">You will have 10 seconds to select your answer.</p>
+        <p className="intro__rules-text">The faster you answer, the more points you will get.</p>
+        <p className="intro__rules-text">Have fun!</p>
       </div>
       <Link to="/game" className="intro__play">
         Play
       </Link>
+      </div>
     </div>
   );
 }
